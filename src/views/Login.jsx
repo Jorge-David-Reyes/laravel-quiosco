210f628d4f7d65a269f0b48df97c6454e44ed1bc
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-09-08 16:04:58 -0600

    263 - Creando la Pagina y Formulario de Autenticacion

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,5 +1,48 @@
 export default function Login() {
   return (
-    <div>Login</div>
+    <>
+      <h1 className="text-4xl font-black">Iniciar Sesion</h1>
+      <p>Para crear un pedido debes iniciar sesion</p>
+
+      <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
+          <form action="">
+
+            <div className="mb-4">
+              <label
+                className="text-slate-800"
+                htmlFor="email"
+              >Email:</label>
+              <input
+                type="email"
+                id="email"
+                className="mt-2 w-full block p-3 bg-gray-50"
+                name="email"
+                placeholder="Tu Email"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="text-slate-800"
+                htmlFor="password"
+              >Password:</label>
+              <input
+                type="password"
+                id="password"
+                className="mt-2 w-full block p-3 bg-gray-50"
+                name="password"
+                placeholder="Tu Password"
+              />
+            </div>
+
+            <input 
+              type="submit" 
+              value="Iniciar Sesion"
+              className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3
+              uppercase font-bold cursor-pointer"
+            />
+          </form>
+      </div>
+    </>
   )
 }
