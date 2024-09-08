e4dfc41e820c89c6c9e70fcb806c51bfa7f1c1d0
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-09-08 16:23:37 -0600

    264 - Navegando por las paginas de Registro y Autenticacion con React Router DOM

--- src/views/Registro.jsx
+++ src/views/Registro.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 export default function Registro() {
   return (
     <>
@@ -70,6 +72,12 @@ export default function Registro() {
               />
             </form>
         </div>
+
+        <nav className="mt-5">
+          <Link to="/auth/login">
+            ¿Ya tienes cuenta? Inicia Sesion
+          </Link>
+        </nav>
     </>
   )
 }
