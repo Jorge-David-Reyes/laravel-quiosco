95ef35db63a62528449e6e382f987dcabc8448d6
Jorge-David-Reyes <A315423@alumnos.uaslp.mx>
2024-09-16 19:05:50 -0600

    295 - Deshabilitar el Boton de Realizar Pedido si el Pedido esta vacio

--- src/components/Resumen.jsx
+++ src/components/Resumen.jsx
@@ -5,6 +5,8 @@ import ResumenProducto from "./ResumenProducto";
 export default function Resumen() {
   const { pedido, total } = useQuiosco();
 
+  const comprobarPedido = () => pedido.length === 0;
+
   return (
     <aside className="w-72 h-screen overflow-y-scroll p-5">
       <h1 className="text-4xl font-black">
@@ -36,8 +38,13 @@ export default function Resumen() {
 
       <form className="w-full">
         <div className="mt-5">
-          <input type="submit" className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 rounded uppercase font-bold text-white text-center w-full cursor-pointer"
-          value="confirmar-pedido"/>
+          <input type="submit" className={`${comprobarPedido() ?
+            'bg-indigo-100' :
+            'bg-indigo-600 hover:bg-indigo-800'
+          } px-5 py-2 rounded uppercase font-bold text-white text-center w-full cursor-pointer`}
+          value="confirmar pedido"
+          disabled={comprobarPedido()}
+          />
         </div>
       </form>
     </aside>
